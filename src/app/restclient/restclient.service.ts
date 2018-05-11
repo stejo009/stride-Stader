de62f57e6e64d0e17fc5d53eee6e5d4dc9bebae9
stejo009 <pudalepramod@gmail.com>
2018-05-11 18:54:01 +0530

    simple response of stadium

--- src/app/restclient/restclient.service.ts
+++ src/app/restclient/restclient.service.ts
@@ -7,20 +7,28 @@ import 'rxjs/Rx';
 @Injectable()
 export class RestclientService {
   alldata;
+  options;
+  testingData = "testind data send to server";
   
-  constructor(private http:Http ) {
+  constructor( private http:Http ) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
     headers.append('Access-Control-Allow-Headers', 'session-variable');
     headers.append('Access-Control-Allow-Methods', 'POST, OPTIONS');
     headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
     headers.append('Access-Control-Allow-Credentials', 'true');
-    let options = new RequestOptions({headers: headers});
+    this.options = new RequestOptions({headers: headers});
    }
    
   //  this function send request to ORDER Request server for Orders Data
  getOrdersData(){
     console.log("inside the client request functions");
+    return  this.http.get("http://phplaravel-68825-370136.cloudwaysapps.com/api/allStadiums");
+   }
+
+// Send get request for client payments data
+  getPaymentData(){
+    console.log("Inside the payment rest request function");
     this.http.get("http://phpstack-68825-403367.cloudwaysapps.com/testApi.php")
     .subscribe(data=>{
     this.alldata = data.json();
@@ -31,19 +39,10 @@ export class RestclientService {
       console.log("Req completed!");
     });
    }
-
-// Send get request for client payments data
-  getPaymentData(){
-      console.log("Inside the payment rest request function");
-    this.http.get("http://phpstack-68825-403367.cloudwaysapps.com/testApi.php")
-    .subscribe(data=>{
-    this.alldata = data.json();
-    console.log(this.alldata);  
-    }, err=>{
-      console.error("Error = " + err);
-    }, ()=>{
-      console.log("Req completed!");
-    });
+   
+  //  post username and password request
+  execPOSTRequest(body){
+    return this.http.post('http://localhost:3000/user', this.options, JSON.stringify(body));
    }
 
 }//Class Completed
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoginService } from '../services/login.service';
+import { RestclientService } from '../restclient/restclient.service';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
+  username :string= "";
+  password :string= "";
+  name;
+  pass;
+  responseMessage;
+  constructor(private RestclientService:RestclientService) { }
 
   ngOnInit() {
   }
 
+  onLoggedin(){
+    // get username and password from user 
+    let uname = this.username;
+    let pass = this.password;
+    console.log("Username:" + uname);
+    console.log("Password:" + pass);
+    let body = {
+    
+      username : uname,
+      password : pass
+  }
+    
+  this.RestclientService.execPOSTRequest(body).subscribe(
+      data => {
+        
+          //disable progress bar
+
+          this.responseMessage = data.json();
+          console.log('SERVER RESPONSE: ' , data.toString);
+          console.log('SERVER RESPONSE: ' , this.responseMessage);
+
+          // this.auth = StringUtils.getAuthHeader(this.username, encryptedPass);
+          // localStorage.setItem("AUTH", this.auth);
+          // localStorage.setItem("USERNAME", this.username);
+          
+          // this.authService.login();
+
+          // dialogRef.close();
+
+          // Get the redirect URL from our auth service
+          // If no redirect has been set, use the default
+          // let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : 'dashboard';
+
+          // this.navigate(redirect);
+      },
+      err => {
+        //show user authentication is failed
+          console.log(err);
+          // dialogRef.close();
+          // super.showAlert("Wrong username or password!");
+      },
+      () => {
+          console.log('Login service completed');             
+      }
+  );
+
+  }
 }
